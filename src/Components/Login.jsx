d038409c6573c17e03eeb065611d0fa8efb33cb6
Arunkumar Rajbhar <arunkbr@gitl.com>
2022-05-27 19:25:52 +0530

    added changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,33 +1,97 @@
-import { Row,Col,Container,Button,Image,Stack} from "react-bootstrap";
-import logo from "../images/Godrej-logo.png"
-import login from "../images/Login.jfif"
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import Logo from "../images/Logo.svg";
+import { Person, Lock, EyeSlash, Eye } from "react-bootstrap-icons";
+import { useState } from "react";
 
-export default function Login({callLogin}) {
+export default function Login({ callLogin }) {
+  const [typePassword, setTypePassword] = useState("password");
 
-    return (
-        <Container className="login-container">
-            <Row>
-                <Col lg={9}><Image className="login-image" src={login}/> </Col>
-                <Col>
-                <Container className="login">
-            <Row>
-                <Col>
-            <Image className="login-Logo" src={logo}/>   
-            <br></br>
-            <br></br>
-            <h1>Customer Request Application</h1>
-            </Col>
-            </Row>
-            <br></br>
-                <br></br>
-                <Row >
-                    <Col>
-                        <Button className="login-btn" onClick={()=> callLogin()}>Login</Button>
-                    </Col>
-                </Row>
-            </Container>
-                </Col>
-            </Row>
-            </Container>
-            )
+  const changeType = () => {
+    if (typePassword === "password") {
+      setTypePassword("text");
+    } else {
+      setTypePassword("password");
+    }
+  };
+
+  return (
+    <Container
+      style={{
+        background:
+          "linear-gradient(to right,  #0D9DD7 0%,#0D9DD7 60%,#FFFFFF 40%,#FFFFFF 100%)",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        maxWidth: "100%",
+      }}
+    >
+      <Row style={{ margin: 0, padding: 0 }}>
+        <Col md={8} style={{ margin: 0, padding: 0 }}></Col>
+        <Col md={4} style={{ margin: 0, padding: 0 }}>
+          <br />
+          <br />
+          <Image src={Logo} style={{ width: "90px", height: "40px" }} />
+          <br />
+          <br />
+          <h1 style={{ fontSize: "3.5rem" }}>Customer Request Application</h1>
+          <br />
+          <br />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              rowGap: "20px",
+            }}
+          >
+            <div style={{ borderBottom: "1px solid #979797", width: "53%" }}>
+              <Person />
+              <input
+                type="text"
+                placeholder="User Name"
+                style={{
+                  border: 0,
+                  outline: 0,
+                  fontSize: "16px",
+                  marginLeft: "10px",
+                  fontWeight: "500",
+                }}
+              ></input>
+            </div>
+
+            <div
+              style={{
+                borderBottom: "1px solid #979797",
+                width: "53%",
+                display: "flex",
+                flexDirection: "row",
+              }}
+            >
+              <Lock />
+              <input
+                type={typePassword}
+                placeholder="Password"
+                style={{
+                  border: 0,
+                  outline: 0,
+                  fontSize: "16px",
+                  marginLeft: "10px",
+                  fontWeight: "500",
+                }}
+              ></input>
+              {typePassword === "password" ? (
+                <EyeSlash onClick={changeType} />
+              ) : (
+                <Eye onClick={changeType} />
+              )}
+            </div>
+          </div>
+          <br />
+          <br />
+          <Button className="login-btn" onClick={() => callLogin()}>
+            Login
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
